38dcef38807618541085bcb41a7f4217d8462363
learn <learn@learn01.localdomain>
2018-11-27 10:56:23 -0800

    tests Devise registration and sign-in flows

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+  respond_to :json  # <- Add this line
+  include ActionController::MimeResponds # <- Add this line
 end
